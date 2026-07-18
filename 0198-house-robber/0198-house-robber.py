0a0f3079201b8ecf78897b5d6ab6c8b1bee37c38
AlanFrancis <Alanfrancis11AH@gmail.com>
2026-07-18 19:06:42 +0530

    Time: 0 ms (100%), Space: 12.3 MB (57.14%) - LeetHub-Neo

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob(self, nums):
+        n = len(nums)
+        dp = [0] * (n)
+        if n <= 1:
+            return nums[0]
+        
+        #Base Case 
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])#don't rob, rob 
+
+        return dp[n-1]
+        
